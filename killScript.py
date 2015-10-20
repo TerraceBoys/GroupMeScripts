b200dc716947cf9274fd9e965c3026025dbe3bbd
Brian Cox <bcox@hubspot.com>
2015-10-19 23:01:50 -0400

    Got members

--- killScript.py
+++ killScript.py
@@ -7,10 +7,14 @@ baseUrl = 'https://api.groupme.com/v3/'
 access_token = '?token=KZZMpZlBXr8b1Td0QTsHGLo7aIbPRWojZRHONX5L'
 
 def main():
-
-    req = urllib2.Request('baseUrl'+'groups/'+access_token, data="", headers={'Content-type': 'application/json'})
+    req = urllib2.Request(baseUrl+'groups/17217125'+access_token, headers={'Content-type': 'application/json'})
     r = urllib2.urlopen(req)
-    print r
+    getMembers(json.load(r))
+
+def getMembers(json):
+    print json
+    for x in range(len(json['response']['members'])):
+        print json['response']['members'][x]
 
 main()
 
