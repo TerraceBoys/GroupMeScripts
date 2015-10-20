19b62600c202204ffdf4d95453dd5763748ab90f
Brian Cox <bcox@hubspot.com>
2015-10-20 11:40:39 -0400

    Idiot proofed it a little bit

--- killScript.py
+++ killScript.py
@@ -11,25 +11,29 @@ members = defaultdict(list)
 
 def main(args):
     global group_name
-    group_name = str(args[1])
-    get_groups = urllib2.Request(baseUrl+'groups'+access_token, headers={'Content-type': 'application/json'})
-    get_groups_resp = urllib2.urlopen(get_groups)
-    old_group_id = getOldGroupId(json.load(get_groups_resp))
-    get_members = urllib2.Request(baseUrl+'groups/'+old_group_id+access_token, headers={'Content-type': 'application/json'})
-    get_members_resp = urllib2.urlopen(get_members)
-    getMembers(json.load(get_members_resp))
-    destroy = urllib2.Request(baseUrl+'groups/'+old_group_id+'/destroy'+access_token, data="", headers={'Content-type': 'application/json'})
-    data = {
-          "name": group_name,
-          "share": True,
-          "image_url": "http://i.groupme.com/123456789"
-        }
-    create = urllib2.Request(baseUrl+'groups'+access_token, data = json.dumps(data), headers={'Content-type': 'application/json'})
-    destroy_resp = urllib2.urlopen(destroy)
-    create_resp = urllib2.urlopen(create)
-    id = getNewGroupId(json.load(create_resp))
-    invite = urllib2.Request(baseUrl+'groups/'+id+'/members/add'+access_token, data=json.dumps(members), headers={'Content-type': 'application/json'})
-    invite_resp = urllib2.urlopen(invite)
+    try:
+        group_name = str(args[1])
+        get_groups = urllib2.Request(baseUrl+'groups'+access_token, headers={'Content-type': 'application/json'})
+        get_groups_resp = urllib2.urlopen(get_groups)
+        old_group_id = getOldGroupId(json.load(get_groups_resp))
+        get_members = urllib2.Request(baseUrl+'groups/'+old_group_id+access_token, headers={'Content-type': 'application/json'})
+        get_members_resp = urllib2.urlopen(get_members)
+        getMembers(json.load(get_members_resp))
+        destroy = urllib2.Request(baseUrl+'groups/'+old_group_id+'/destroy'+access_token, data="", headers={'Content-type': 'application/json'})
+        data = {
+              "name": group_name,
+              "share": True,
+              "image_url": "http://i.groupme.com/123456789"
+            }
+        create = urllib2.Request(baseUrl+'groups'+access_token, data = json.dumps(data), headers={'Content-type': 'application/json'})
+        destroy_resp = urllib2.urlopen(destroy)
+        create_resp = urllib2.urlopen(create)
+        id = getNewGroupId(json.load(create_resp))
+        invite = urllib2.Request(baseUrl+'groups/'+id+'/members/add'+access_token, data=json.dumps(members), headers={'Content-type': 'application/json'})
+        invite_resp = urllib2.urlopen(invite)
+
+    except TypeError:
+        print "Check your spelling, Julian"
 
 def getOldGroupId(get_groups_resp):
     for x in get_groups_resp['response']:
